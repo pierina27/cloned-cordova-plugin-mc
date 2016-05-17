c6ff78062064fe984e36afae3fce23b84c58bfac
Felipe <fechanique@leadclic.com>
2016-05-17 11:13:36 +0200

    fixed bug

--- src/android/MCPluginApplication.java
+++ src/android/MCPluginApplication.java
@@ -56,7 +56,7 @@ public class MCPluginApplication extends Application {
 		try {
 			etPush = event.getEtPush();
 			etPush.addTag("2.1.0");
-			etPush.pushManager.setNotificationResourceId(R.drawable.ic_stat_sdk_explorer);
+			etPush.setNotificationResourceId(R.drawable.ic_stat_sdk_explorer);
 		} catch (ETException e) {
 			Log.d(TAG, "ERROR onReadyAimFireInitCompletedEvent: " + e.getMessage());
 		}
