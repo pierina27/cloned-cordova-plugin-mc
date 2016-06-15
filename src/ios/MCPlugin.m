3ab65bf03e75f92ab79b44aac1bb938c2a57aeb5
soporteleadclic <kmontoya@leadclic.com>
2016-06-15 12:10:40 +0200

    notification response as json object

--- src/ios/MCPlugin.m
+++ src/ios/MCPlugin.m
@@ -180,7 +180,7 @@ static MCPlugin *etPluginInstance;
 -(void) notifyOfMessage:(NSData *)payload
 {
     NSString *JSONString = [[NSString alloc] initWithBytes:[payload bytes] length:[payload length] encoding:NSUTF8StringEncoding];
-    NSString * notifyJS = [NSString stringWithFormat:@"%@('%@');", notificationCallback, JSONString];
+    NSString * notifyJS = [NSString stringWithFormat:@"%@(%@);", notificationCallback, JSONString];
     NSLog(@"stringByEvaluatingJavaScriptFromString %@", notifyJS);
     
     NSString *jsResults = [self.webView stringByEvaluatingJavaScriptFromString:notifyJS];
